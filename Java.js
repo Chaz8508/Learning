ca072738e259cd690742244fdadabb73e31fa626
Chaz8508 <139968996+Chaz8508@users.noreply.github.com>
2023-07-19 15:23:47 +0100

    Update Java.js
    Updated

--- Java.js
+++ Java.js
@@ -13,3 +13,16 @@ const Num7 = 8 / 4; // JS uses / for division
 Num1++; // Not really 100% sure on this code not very well explained all i know is it adds 'one'
 Num1--; // It also works the same with with Taking 'one' away
 let Decimal = 5.3; // These are called floats (Decimal)
+
+//Assignements 
+
+// If we have a varible such 
+let Num8 = 2; //This Number 8 Varible = 2, due to it being a let varible we can add to it.
+Num8 += 6; //This is a assignment we are assigning a vaule to that varible seeing as that varible already = 2 adding 6 to it makes it 8
+console.log (Num8); // we can Print this Varible in the Console to check the new Value 
+
+// This also works both ways for Taking away -
+let Num9 = 8;
+Num9 -= 4;
+console.log (Num9)
+// For example see above if this was put into a console the vaule would be = 4 
