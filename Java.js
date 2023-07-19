9c57972cc1ca4e8b04368546321c695976922d47
Chaz8508 <139968996+Chaz8508@users.noreply.github.com>
2023-07-19 11:57:05 +0100

    Add files via upload
    Started Learning Java Script

---
+++ Java.js
@@ -0,0 +1,15 @@
+console.log = "Hello"; 
+// I will be learning Java  
+var Num1 = 1;
+var Num2 = 5;// You Can assing Varibles Diffrent values 
+var A = Num1 + Num2;// Inputting a Data Type Varible setting the name of that to Num1 and the value as 1 
+var name ="Charlie"; // Here i can input a string varible using the quote marks 
+console.log = Num1;
+let Num3 = 10; // Let is another way or adding a varible to the code, it allows you to change the varible 
+const Num4 = 15; // Const does the same thing but it does not allow you to change the varible it is CONSTANT
+const Num5 = 12 - 6; // You can also use maths to workout the vaule of a varible
+const Num6 = 8 * 0; // JS uses the * symbol to multiply the data
+const Num7 = 8 / 4; // JS uses / for division 
+Num1++; // Not really 100% sure on this code not very well explained all i know is it adds 'one'
+Num1--; // It also works the same with with Taking 'one' away
+let Decimal = 5.3; // These are called floats (Decimal)
