46d8cfbfe16f212da135df1b654f84f90fd4cefa
Chaz8508 <chazcam8508@gmail.com>
2023-09-01 14:38:08 +0100

    Update
    Added New Sorting Files and New Learning Resources

---
+++ Learn JS/Learn.js
@@ -0,0 +1,3 @@
+var tempFahrenheit =99; //var can be changed and edited 
+const Normal_Fahrenheit = 98.6; //const cannot be changed
+const contry ="United Kingdom"; //Loads of data can be used such as number, strings or boolean 
