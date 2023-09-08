0e156f77fda0a7f1b854264abd899a2014cad3d6
Chaz8508 <chazcam8508@gmail.com>
2023-09-08 11:34:47 +0100

    Update
    Updated Java and My business page adding all the CSS internally having as external doesn't seem to work.
    
    css deleted

--- Learn JS/Learn.js
+++ Learn JS/Learn.js
@@ -1,3 +1,33 @@
+// Varibles 
 var tempFahrenheit =99; //var can be changed and edited 
 const Normal_Fahrenheit = 98.6; //const cannot be changed
 const contry ="United Kingdom"; //Loads of data can be used such as number, strings or boolean 
+
+var cheese ="yummy";
+cheese = "Yummy Yummy";
+const cheese2 ="Yummy";
+
+// Arrays 
+var cake = new Array ("Sugar","Butter","Milk")
+cake[0]; // this will retrieve Sugar 1 butter 2 milk
+var cake2 = ["Coco","dough","Tomatos"];
+cake2[1]; // this will retrieve dough
+cake.length; // this will return 3 seeing as there are 3 objects in the array 
+
+var vehicles = ["Ford" ["Fiesta","Fushion","Explorer"]]
+var myCar = vehicles[2];
+console.log(myCar); // this will say Ford Explorer
+var animals = ["cat","dog","chicken"];
+animals.push("lamar") // ADD this will add a 4th animal to the array 
+animals.splice("1,1") // DELETE this will take dog out of the array 
+// there are alot of array methods you can use just by doing .splice as a example such as sort loads of stuff 
+var animals = ["cat","dog","chicken"];
+animals.length;
+// 4 
+
+var traningMatrix = ["Charlie Matthews", "Jack Newby", "Paul Davies", 
+                     "Siemens UK" , "Electronics" , "Electrical Mechanical",
+                     "Relays", "Goods in", "ETCS"];
+
+var first_day                    
+
