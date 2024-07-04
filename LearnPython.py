1a752a874a83eb699699a93f6b1f2969ed980e96
Chaz8508 <chazcam8508@gmail.com>
2024-07-04 10:38:58 +0100

    Updated Learning Resources
    Learning HTML/CSS/Python

---
+++ LearnPython.py
@@ -0,0 +1,9 @@
+print ("Hello This is Where I will Learning Python")
+#~ Python can be used for loads of things, it can do basic maths 
+Num1 = 2 
+Num2 = 4
+Num1 + Num2 = 6 
+#~ can even be done in easier ways as below 
+2 + 2 = 4
+import turtle
+#~ python can do so many things as it has so many importable things using the import command, as we can see here turtle is a drawing bot
